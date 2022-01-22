f7d4c88a02d35326d86106b1a570fc4ad249917b
Ricardo Chapa <ricardochaparomero@gmail.com>
2022-01-21 20:06:46 -0600

    Add front end react app

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SoccerFantasy
+
+## Build Process
+1) Run npm start on server
+2) Run npm start on client
+  i) In order to see changes from the server to the client refresh the page
--- server/index.js
+++ server/index.js
@@ -1,10 +1,29 @@
 // server/index.js
 const express = require("express");
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(express.json());
+/**
+UNCOMMENT UNTIL DEPLOYMENT
 
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+*/
+
+// Methods
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server 2!" });
+});
+
+// Listener
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });

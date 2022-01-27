ffd80a541e382a2a769e181a215db074138c0330
zJuarez <mji2000.741@gmail.com>
2022-01-26 21:06:29 -0600

    avance team UI

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import StartPage from './components/start/StartPage';
+import Field from './components/team/Field';
 
 function App() {
   const pages = ['team', 'social', 'stats', 'about'];
@@ -16,7 +17,7 @@ function App() {
       <Navbar pages={pages}> </Navbar>
       <Routes>
         <Route path="/" element={<h1> home</h1>} />
-        <Route path="/team" element={<h1> team</h1>} />
+        <Route path="/team" element={<Field />} />
         <Route path="/social" element={<h1> social</h1>} />
         <Route path="/stats" element={<h1> stats</h1>} />
         <Route path="/about" element={<h1> about</h1>} />

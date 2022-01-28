c5ca00451020bf7adb90da6e39969c279a056e0e
zJuarez <mji2000.741@gmail.com>
2022-01-27 18:25:09 -0600

    field con fomracion

--- client/src/components/team/Field.js
+++ client/src/components/team/Field.js
@@ -1,40 +1,44 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import './Field.css';
-import Box from '@mui/material/Box';
+import Player from './Player';
 
 const Field = (props) => {
-  const { players } = props;
+  const { formation } = props;
+  console.log(formation);
   return (
-    <div className="field">
-      <Box
-        noWrap
-        sx={{
-          display: 'flex',
-          height: '20%'
-        }}
-      ></Box>
-      <Box
-        noWrap
-        sx={{
-          display: { xs: 'none', md: 'flex' }
-        }}
-      ></Box>
-      <Box
-        noWrap
-        sx={{
-          display: { xs: 'none', md: 'flex' }
-        }}
-      ></Box>
+    <div className="container">
+      <div className="field">
+        <div className="line">
+          {formation[2].map((i, player) => (
+            <Player key={i} />
+          ))}
+        </div>
+        <div className="line">
+          {formation[1].map((i, player) => (
+            <Player key={i} />
+          ))}
+        </div>
+        <div className="line">
+          {formation[0].map((i, player) => (
+            <Player key={i} />
+          ))}
+        </div>
+        <div className="line">
+          <Player />
+        </div>
+      </div>
     </div>
   );
 };
 Field.propTypes = {
-  players: PropTypes.array,
   formation: PropTypes.array
 };
 Field.defaultProps = {
-  players: [],
-  formation: [4, 3, 3]
+  formation: [
+    [1, 2, 3, 4],
+    [1, 2, 3],
+    [1, 2, 3]
+  ]
 };
 export default Field;
---
+++ client/src/components/team/Player.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import './Player.css';
+
+const Player = (props) => {
+  return <div className="player"></div>;
+};
+
+export default Player;

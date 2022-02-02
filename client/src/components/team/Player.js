42005bbfa3f386354add538f6afd83cdf1676da1
zJuarez <mji2000.741@gmail.com>
2022-02-02 14:28:40 -0600

    created render for player on field (/team)

--- client/src/components/team/Player.js
+++ client/src/components/team/Player.js
@@ -1,8 +1,60 @@
 import * as React from 'react';
-import './Player.css';
+import PropTypes from 'prop-types';
+import Avatar from '@mui/material/Avatar';
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import colors from '../../constants/colors';
+import Tooltip from '@mui/material/Tooltip';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import { getColor } from '../../constants/positions';
+import { stringAvatar, size } from '../../constants/avatars';
 
 const Player = (props) => {
-  return <div className="player"></div>;
-};
+  const { playerName, playerPhoto, position } = props;
+  // default render
+  if (playerName === '' && playerPhoto === '') {
+    return (
+      <Tooltip title="Add a player!" placement="top">
+        <Avatar
+          sx={{
+            bgcolor: colors.darkGray,
+            width: size,
+            height: size
+          }}
+        ></Avatar>
+      </Tooltip>
+    );
+  }
 
+  const playerAvatar =
+    playerPhoto === '' || playerPhoto == null ? (
+      <Avatar {...stringAvatar(playerName, size)} />
+    ) : (
+      <Avatar
+        alt={playerName}
+        src={playerPhoto}
+        sx={{ width: size, height: size }}
+      />
+    );
+  return (
+    <Stack
+      direction="column"
+      spacing={{ md: 2, xs: 1 }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      {playerAvatar}
+      <Chip color={getColor(position)} label={playerName} />
+    </Stack>
+  );
+};
+Player.propTypes = {
+  playerName: PropTypes.string,
+  playerPhoto: PropTypes.string,
+  position: PropTypes.string
+};
+Player.defaultProps = {
+  playerName: '',
+  playerPhoto: ''
+};
 export default Player;
